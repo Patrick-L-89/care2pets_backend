e7c5eb7e195a051e616e858d0b0f6e22eecd38d4
patrick-coda <louter.p.h.a@gmail.com>
2020-09-07 14:15:01 +0200

    added the reciever for the frontend so the backend knows when a message is sent

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ io.on("connection", (socket) => {
     callback();
   });
 
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
   socket.on("disconnect", () => {
     console.log("user has left the chat");
   });
